880a29e5dfedbff7b975cbe45279868db34900b0
matthewrockwood <rockwoodmatthew@gmail.com>
2025-02-19 21:47:33 -0500

    Made A functional NavBar that routes to all other pages

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,8 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 //import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
+import Women from './pages/womens-page'
+import Men from './pages/mens-page'
+import SignIn from './pages/sign-in'
+import Checkout from './pages/check-out'
+import About from './pages/about'
 
 function App() {
   
@@ -11,11 +16,16 @@ function App() {
   return (
     <div>
       
-      <BrowserRouter>
+      
       <Routes>
         <Route path = "/" index element = {<Home />} />
+        <Route path = "/about" index element = {<About />} />
+        <Route path = "/womens-page" index element = {<Women />} />
+        <Route path = "/mens-page" index element = {<Men />} />
+        <Route path = "/sign-in" index element = {<SignIn />} />
+        <Route path = "/check-out" index element = {<Checkout />} />
       </Routes>
-      </BrowserRouter>
+      
       
       
     </div>
--- my-app/src/pages/home.jsx
+++ my-app/src/pages/home.jsx
@@ -1,9 +1,10 @@
-import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import "./Home.css"
 
 export default function Home(){
     return(
     <>
-    <Header />
+    <Navbar />
     <h2>Home Page</h2>
     </>
     )
