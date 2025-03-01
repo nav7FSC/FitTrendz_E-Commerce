38f92628afdc265db585635db99156a4112e426a
matthewrockwood <rockwoodmatthew@gmail.com>
2025-02-28 23:24:42 -0500

    fixed the footer.(for the most part if the page is empty then its a little weird but we wont have any empty pages)

--- my-app/src/pages/about.jsx
+++ my-app/src/pages/about.jsx
@@ -6,9 +6,11 @@ import Footer from "../components/footer";
 export default function About(){
     return(
         <>
+        <div>
         
         <Navbar />
         <h2>About page</h2>
+        </div>
         <Footer />
         </>
     )
